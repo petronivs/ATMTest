ef87ee17c11ec50f79b736b35e497bf595330c23
Adam Miller <amiller@proshipinc.com>
2019-04-09 18:44:35 -0500

    First commit

---
+++ ATMTest/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ATMTest
+{
+    public class ATMClass
+    {
+        private Dictionary<String, ATMDrawer> CashDrawers;
+
+        public ATMClass()
+        {
+            CashDrawers = new Dictionary<string, ATMDrawer>();
+        }
+    }
+
+    internal class ATMDrawer
+    {
+        public string code { get; set; }
+        public int value { get; set; }
+        public int inventory { get; set; }
+    }
+}
